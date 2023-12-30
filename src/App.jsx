4fb6625d4072ba17b808d55c060427b9b86abd28
Gkaav <gigikavelashvili@gmail.com>
2023-12-30 16:08:23 +0400

    project starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p>dwasd</p>
+      <div>
+        <Header/>
+      </div>
     </>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+function Header() {
+  return (
+    <>
+      <div>
+        <h1>sneakers</h1>
+        <div>
+          <ul>
+            <li>
+              <a href="*">Collections</a>
+            </li>
+            <li>
+              <a href="*">Men</a>
+            </li>
+            <li>
+              <a href="*">Women</a>
+            </li>
+            <li>
+              <a href="*">About</a>
+            </li>
+            <li>
+              <a href="*">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <img src="./src/assets/icon-cart.svg" alt="" className="cart-img" />
+      </div>
+    </>
+  );
+}
+
+export default Header;
