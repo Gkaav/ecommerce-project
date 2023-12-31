cf4cd2171b852b804574040ad43fd4ff5a13e626
Gkaav <gigikavelashvili@gmail.com>
2023-12-31 16:59:18 +0400

    header building

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,28 +1,53 @@
 function Header() {
   return (
     <>
-      <div>
-        <h1>sneakers</h1>
-        <div>
-          <ul>
-            <li>
-              <a href="*">Collections</a>
-            </li>
-            <li>
-              <a href="*">Men</a>
-            </li>
-            <li>
-              <a href="*">Women</a>
-            </li>
-            <li>
-              <a href="*">About</a>
-            </li>
-            <li>
-              <a href="*">Contact</a>
-            </li>
-          </ul>
+      <div className="header">
+        <div className="header-container">
+          <div>
+            <img src="./src/assets/logo.svg" alt="" className="header-logo" />
+          </div>
+          <div className="header-categories">
+            <ul>
+              <li>
+                <div className="header-categories-box">
+                  <a href="*">Collections</a>
+                </div>
+              </li>
+              <li>
+                <div className="header-categories-box">
+                  <a href="*">Men</a>
+                </div>
+              </li>
+              <li>
+                <div className="header-categories-box">
+                  <a href="*">Women</a>
+                </div>
+              </li>
+              <li>
+                <div className="header-categories-box">
+                  <a href="*">About</a>
+                </div>
+              </li>
+              <li>
+                <div className="header-categories-box">
+                  <a href="*">Contact</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="header-img">
+            <img
+              src="./src/assets/icon-cart.svg"
+              alt=""
+              className="header-cart"
+            />
+            <img
+              src="./src/assets/user-placeholder.png"
+              alt=""
+              className="header-user"
+            />
+          </div>
         </div>
-        <img src="./src/assets/icon-cart.svg" alt="" className="cart-img" />
       </div>
     </>
   );
